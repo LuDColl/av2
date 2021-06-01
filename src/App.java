a539f675a4b9fb3f6df9d9b97b10a935c39d0424
LuDColl <lucas.guimaraes.lc@gmail.com>
2021-05-31 21:21:07 -0300

    Menu criado, falta adicionar parâmetros

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        MenuList menu = new MenuList("Banco de Dados");
+        menu.addItem("Incluir");
+        menu.addItem("Alterar");
+        menu.addItem("Excluir");
+        menu.addItem("Consultar");
+
+        boolean run = true;
+        while (run) {
+            menu.run();
+            switch (menu.getItemSelected()) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 0:
+                    run = false;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
